90fb7627637c7d931d98c56bc14b42ee02074468
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-02-27 17:54:38 +0000

    player can move inside an arena

--- game.py
+++ game.py
@@ -1,43 +1,81 @@
 import pygame
 from sys import exit
 
-win = pygame.display.set_mode((500, 500))
+
+#initializes the screen 500x500
+screen = pygame.display.set_mode((500, 500))
+
+#inits game
 pygame.get_init()
 
+#sets window caption
 pygame.display.set_caption("Box World")
 
-x = 20
-y = 20
-width = 40
-height = 60
-vel = 10
+# Creates a clock object to keep track of time
+clock = pygame.time.Clock()
+
+#player 
+player = pygame.Rect(300, 175, 25, 25)
 
 run = True
+#while loop
 while run:
-    pygame.time.delay(100)
+    
+    #press delay for increased playability
+    pygame.time.delay(75)
 
+    # creates the arena 
+    arena = (
+        pygame.Rect(100, 125, 250, 25),
+        pygame.Rect(100, 125, 25, 125),
+        pygame.Rect(350, 125, 25, 125),
+        pygame.Rect(100, 225, 250, 25)
+    )
+
+    
+    #adds key functionality
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
 
+    #creates movement vector that is later used for checking arena boundaries
+    movement = pygame.Vector2(0, 0)
+
     if keys[pygame.K_LEFT]:
-        x -= vel
+        movement.x -= 25
 
     if keys[pygame.K_RIGHT]:
-        x += vel
+        movement.x += 25
 
     if keys[pygame.K_UP]:
-        y -= vel
+        movement.y -= 25
 
     if keys[pygame.K_DOWN]:
-        y += vel
+        movement.y += 25
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    #checks if player does not go beyond arena boundaries
+    if movement.x != 0 or movement.y != 0:
+        rect = screen.get_rect().inflate(-6, -6)
+        moved = player.move(movement.x, movement.y)
+        if moved.collidelist(arena) == -1 and rect.contains(moved):
+            player = moved
 
-    pygame.display.update()
+    screen.fill((0,0,0))
+
+    #draws arena
+    for wall in arena:
+        pygame.draw.rect(screen, (0, 255, 0), (wall.x, wall.y, wall.width, wall.height))
+
+    #draws player
+    pygame.draw.rect(screen, pygame.Color('red'), player)
+
+    # Update the full Surface to the screen
+    pygame.display.flip()
+
+    # Run the program at 60 frames per second
+    clock.tick(60)
 
 pygame.quit()
 
