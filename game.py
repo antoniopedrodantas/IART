ffbf0c98a07865feb7921f0ce447d6116a581ed5
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-03-04 17:17:45 +0000

    First playable version

--- game.py
+++ game.py
@@ -18,21 +18,33 @@ clock = pygame.time.Clock()
 player = pygame.Rect(325, 175, 25, 25)
 
 #box
-box = pygame.Rect(200, 175, 25, 25)
+boxes = (
+    pygame.Rect(175, 150, 25, 25),
+    pygame.Rect(200, 175, 25, 25),
+    pygame.Rect(225, 200, 25, 25),
+    pygame.Rect(125, 150, 25, 25),
+    pygame.Rect(150, 175, 25, 25),
+    pygame.Rect(175, 200, 25, 25),
+)
+
+finish = pygame.Rect(100, 175, 25, 25)
+
 
 run = True
 #while loop
 while run:
     
     #press delay for increased playability
-    pygame.time.delay(75)
+    pygame.time.delay(90)
 
     # creates the arena 
     arena = (
-        pygame.Rect(100, 125, 250, 25),
-        pygame.Rect(100, 125, 25, 125),
+        pygame.Rect(75, 125, 300, 25),
+        pygame.Rect(75, 125, 25, 125),
         pygame.Rect(350, 125, 25, 125),
-        pygame.Rect(100, 225, 250, 25)
+        pygame.Rect(100, 225, 250, 25),
+        pygame.Rect(100, 150, 25, 25),
+        pygame.Rect(100, 200, 25, 25)
     )
 
     
@@ -58,27 +70,42 @@ while run:
     if keys[pygame.K_DOWN]:
         movement_player.y += 25
 
+    if keys[pygame.K_ESCAPE]:
+        run = False
+
     #checks if player does not go beyond arena boundaries
     if movement_player.x != 0 or movement_player.y != 0:
         rect = screen.get_rect().inflate(-6, -6)
         moved = player.move(movement_player.x, movement_player.y)
-        box_moved = box.move(0, 0)
+        for box in boxes:
+            box_moved = moved
         #if the player is inside the arena he can move
         if moved.collidelist(arena) == -1 and box_moved.collidelist(arena) == -1:
             player = moved
-            #if the player collides with the box it moves
-            if player.colliderect(box):
-                box_moved = box.move(movement_player.x, movement_player.y)
-                #if the box collides with the arena it does not move
-                if box_moved.collidelist(arena) == -1:
-                    box = box_moved
-                else:
-                    player.x -= movement_player.x
-                    player.y -= movement_player.y
+
+            #winning mechanism
+            if player.colliderect(finish):
+                print("YOU WON!")
+                run = False
+
+            for box in boxes:
+                #if the player collides any box it moves
+                if player.colliderect(box):
+                    box_moved = box.move(movement_player.x, movement_player.y)
+                    #if the box collides with the arena or another box it does not move
+                    if box_moved.collidelist(arena) == -1 and box_moved.collidelist(boxes) == -1:
+                        box.x += movement_player.x
+                        box.y += movement_player.y
+                    else:
+                        player.x -= movement_player.x
+                        player.y -= movement_player.y
 
 
 
     screen.fill((0,0,0))
+
+    #draws finish
+    pygame.draw.rect(screen, pygame.Color('blue'), finish)
 
     #draws arena
     for wall in arena:
@@ -88,7 +115,8 @@ while run:
     pygame.draw.rect(screen, pygame.Color('red'), player)
 
     #draws box
-    pygame.draw.rect(screen, pygame.Color('yellow'), box)
+    for box in boxes:
+        pygame.draw.rect(screen, pygame.Color('yellow'), box)
 
     # Update the full Surface to the screen
     pygame.display.flip()
