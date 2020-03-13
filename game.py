9995119c22f24f854cd6433123767c653899d522
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-03-13 15:22:40 +0000

    started level2

--- game.py
+++ game.py
@@ -15,9 +15,16 @@ pygame.display.set_caption("Box World")
 clock = pygame.time.Clock()
 
 #player 
-player = pygame.Rect(325, 175, 25, 25)
+#level1
+#player = pygame.Rect(325, 175, 25, 25)
+
+#level2
+player = pygame.Rect(75, 175, 25, 25)
+
 
 #boxes
+"""
+#level1
 boxes = (
     pygame.Rect(175, 150, 25, 25),
     pygame.Rect(200, 175, 25, 25),
@@ -26,16 +33,63 @@ boxes = (
     pygame.Rect(150, 175, 25, 25),
     pygame.Rect(175, 200, 25, 25),
 )
+"""
+#level2
+boxes = (
+    pygame.Rect(125, 125, 25, 25),
+    pygame.Rect(150, 125, 25, 25),
+    pygame.Rect(175, 125, 25, 25),
+    pygame.Rect(200, 125, 25, 25),
+    pygame.Rect(150, 150, 25, 25),
+    pygame.Rect(200, 150, 25, 25),
+)
+
 
 #game floor
+"""
+#level1
 floor = (
     pygame.Rect(125, 200, 225, 25),
     pygame.Rect(125, 150, 225, 25),
     pygame.Rect(125, 175, 225, 25),
 )
+"""
+
+#arena
+"""
+#level1
+arena = (
+    pygame.Rect(75, 125, 300, 25),
+    pygame.Rect(75, 125, 25, 125),
+    pygame.Rect(350, 125, 25, 125),
+    pygame.Rect(100, 225, 250, 25),
+    pygame.Rect(100, 150, 25, 25),
+    pygame.Rect(100, 200, 25, 25)
+)
+"""
+#level2
+arena = (
+    pygame.Rect(50, 50, 250, 25),
+    pygame.Rect(50, 75, 25, 25),
+    pygame.Rect(275, 75, 25, 25),
+    pygame.Rect(50, 100, 50, 25),
+    pygame.Rect(250, 100, 50, 25),
+    pygame.Rect(50, 125, 75, 25),
+    pygame.Rect(225, 125, 75, 25),
+    pygame.Rect(50, 150, 50, 25),
+    pygame.Rect(250, 150, 50, 25),
+    pygame.Rect(275, 175, 25, 25),
+    pygame.Rect(50, 175, 25, 25),
+    pygame.Rect(50, 200, 250, 25),
+)
+    
     
 #finish station
-finish = pygame.Rect(100, 175, 25, 25)
+#level1
+#finish = pygame.Rect(100, 175, 25, 25)
+
+#level2
+finish = pygame.Rect(75, 75, 25, 25)
 
 #while loop
 run = True
@@ -44,16 +98,6 @@ while run:
     #press delay for increased playability
     pygame.time.delay(90)
 
-    #arena
-    arena = (
-        pygame.Rect(75, 125, 300, 25),
-        pygame.Rect(75, 125, 25, 125),
-        pygame.Rect(350, 125, 25, 125),
-        pygame.Rect(100, 225, 250, 25),
-        pygame.Rect(100, 150, 25, 25),
-        pygame.Rect(100, 200, 25, 25)
-    )
-    
     #adds key functionality
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -119,8 +163,8 @@ while run:
 
     
     #draws floor
-    for tile in floor:
-        pygame.draw.rect(screen, pygame.Color('pink'), tile)
+    #for tile in floor:
+        #pygame.draw.rect(screen, pygame.Color('pink'), tile)
     
 
     #draws player
