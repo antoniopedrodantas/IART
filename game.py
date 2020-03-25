1d476819958de178f9e33f6bd41f424c717e612b
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-03-25 16:52:05 +0000

    started AI

--- game.py
+++ game.py
@@ -4,93 +4,98 @@ import argparse
 from sys import exit
 
 from level import *
+from state import *
 
 
 
 def move(movement_player, movement):
     if(movement == "left"):
         movement_player.x -= 25
+        return movement_player
     elif(movement == "right"):
         movement_player.x += 25
+        return movement_player
     elif(movement == "up"):
         movement_player.y -= 25
+        return movement_player
     elif(movement == "down"):
-        movement_player.y += 25  
+        movement_player.y += 25
+        return movement_player  
 
 
 
-def calculateGameState(level):
+def calculateGameState(movement, st):
 
     #checks if player does not go beyond arena boundaries
-    if movement_player.x != 0 or movement_player.y != 0:
+    if movement.x != 0 or movement.y != 0:
         rect = screen.get_rect().inflate(-6, -6)
-        moved = level.player.move(movement_player.x, movement_player.y)
+        moved = st.player.move(movement.x, movement.y)
         
-        for box in level.boxes:
+        for box in st.boxes:
             box_moved = moved
 
-        for ice in level.iceBoxes:
+        for ice in st.iceBoxes:
             ice_moved = moved
         
         #if the player is inside the arena he can move
-        if moved.collidelist(level.arena) == -1 and box_moved.collidelist(level.arena) == -1:
-            level.player = moved
+        if moved.collidelist(st.arena) == -1 and box_moved.collidelist(st.arena) == -1:
+            st.player = moved
 
-            for box in level.boxes:
+            for box in st.boxes:
                 #if the player collides any box it moves
-                if level.player.colliderect(box):
-                    box_moved = box.move(movement_player.x, movement_player.y)
+                if st.player.colliderect(box):
+                    box_moved = box.move(movement.x, movement.y)
                     #if the box collides with the arena or another box it does not move
-                    if box_moved.collidelist(level.arena) == -1 and box_moved.collidelist(level.boxes) == -1 and box_moved.collidelist(level.iceBoxes) == -1:
-                        box.x += movement_player.x
-                        box.y += movement_player.y
+                    if box_moved.collidelist(st.arena) == -1 and box_moved.collidelist(st.boxes) == -1 and box_moved.collidelist(st.iceBoxes) == -1:
+                        box.x += movement.x
+                        box.y += movement.y
                     else:
-                        level.player.x -= movement_player.x
-                        level.player.y -= movement_player.y
+                        st.player.x -= movement.x
+                        st.player.y -= movement.y
 
-            for hole in level.holes:
+            for hole in st.holes:
                 #if the player collides any box it moves
-                if level.player.colliderect(hole):
+                if st.player.colliderect(hole):
                     #return False
-                    level.player.x -= movement_player.x
-                    level.player.y -= movement_player.y
+                    st.player.x -= movement.x
+                    st.player.y -= movement.y
 
             #ice
-            for ice in level.iceBoxes:
-                if level.player.colliderect(ice):
-                    ice_moved = ice.move(movement_player.x, movement_player.y)
+            for ice in st.iceBoxes:
+                if st.player.colliderect(ice):
+                    ice_moved = ice.move(movement.x, movement.y)
                     #check when icebox collides
-                    while(ice.collidelist(level.arena) == -1 and ice.collidelist(level.boxes) == -1 and ice.collidelist(level.holes) == -1):
-                        ice.x += movement_player.x
-                        ice.y += movement_player.y
+                    while(ice.collidelist(st.arena) == -1 and ice.collidelist(st.boxes) == -1 and ice.collidelist(st.holes) == -1):
+                        ice.x += movement.x
+                        ice.y += movement.y
                     
-                    if(ice.collidelist(level.holes) != -1):
-                        level.player.x -= movement_player.x
-                        level.player.y -= movement_player.y
+                    if(ice.collidelist(st.holes) != -1):
+                        st.player.x -= movement.x
+                        st.player.y -= movement.y
                     else:
                         #compensating for last move
-                        ice.x -= movement_player.x
-                        ice.y -= movement_player.y
-                        level.player.x -= movement_player.x
-                        level.player.y -= movement_player.y
+                        ice.x -= movement.x
+                        ice.y -= movement.y
+                        st.player.x -= movement.x
+                        st.player.y -= movement.y
 
-            del_holes = level.holes
-            del_boxes = level.boxes
-            del_ice = level.iceBoxes
+            del_holes = st.holes
+            del_boxes = st.boxes
+            del_ice = st.iceBoxes
 
             for i in del_holes:
                 for k in del_boxes:
                     if i.colliderect(k):
-                        level.boxes.remove(k)
-                        level.holes.remove(i)
+                        st.boxes.remove(k)
+                        st.holes.remove(i)
                 for j in del_ice:
                     if i.colliderect(j):
-                        level.iceBoxes.remove(j)
-                        level.holes.remove(i)
+                        st.iceBoxes.remove(j)
+                        st.holes.remove(i)
 
 
             #winning mechanism
-            if level.player.colliderect(level.finish):
+            if st.player.colliderect(st.finish):
                 print("\n")
                 print("YOU WON!")
                 return False
@@ -127,6 +132,57 @@ def drawGameState(level):
         pygame.draw.rect(screen, pygame.Color(0, 255, 255), ice)
 
 
+def nextMove():
+
+    #creates movement
+    movement = pygame.Vector2(0, 0)
+
+    #creates list with possible states
+    possibleStates = []
+
+
+    #creates all tmp variables
+    tmp1 = State(state.level)
+
+    tmp2 = State(state.level)
+
+    tmp3 = State(state.level)
+
+    tmp4 = State(state.level)
+
+    # is it left?
+    movement = move(movement, "left")
+    calculateGameState(movement, tmp1.level)
+    tmp1.addMove("left")
+    possibleStates.append(tmp1)
+
+    # is it right?
+    movement = move(movement, "right")
+    calculateGameState(movement, tmp2.level)
+    tmp2.addMove("right")
+    possibleStates.append(tmp2)
+
+    # is it up?
+    movement = move(movement, "up")
+    calculateGameState(movement, tmp3.level)
+    tmp3.addMove("up")
+    possibleStates.append(tmp3)
+
+    # is it down?
+    movement = move(movement, "down")
+    calculateGameState(movement, tmp4.level)
+    tmp4.addMove("down")
+    possibleStates.append(tmp4)
+
+    for st in possibleStates:
+        queue.append(st)
+        
+
+
+
+
+
+
 
 #initializes the screen 500x500
 screen = pygame.display.set_mode((500, 500))
@@ -149,12 +205,25 @@ l = args.level
 #specifies the level
 level = Level(l)
 
+#game states the player has already been in
+visited = []
+
+#current game state
+state = State(level)
+
+#queue with nodes
+queue = []
+
+#initial state added to the queue
+queue.append(state)
+
+
 #while loop
 run = True
 while True:
     
-    #press delay for increased playability
-    pygame.time.delay(90)
+    
+    state = queue[0]
 
     #adds key functionality
     for event in pygame.event.get():
@@ -164,9 +233,10 @@ while True:
     keys = pygame.key.get_pressed()
 
     #creates movement vector that is later used for checking arena boundaries
-    movement_player = pygame.Vector2(0, 0)
+    #movement_player = pygame.Vector2(0, 0)
 
     #calculates player movement
+    """
     if keys[pygame.K_LEFT]:
         move(movement_player, "left")
 
@@ -178,27 +248,51 @@ while True:
 
     if keys[pygame.K_DOWN]:
         move(movement_player, "down")
+    """
 
     if keys[pygame.K_r]:
         level = Level(l)
 
     if keys[pygame.K_ESCAPE]:
         run = False
+    
 
-    #calculates game state
-    if calculateGameState(level) == False:
-        run = False
 
-    screen.fill((0,0,0))
+    #calculates next move
+    nextMove()
+
+
+    #press delay for increased playability
+    pygame.time.delay(90)
 
     #draws game state
-    drawGameState(level)
+    drawGameState(queue[0].level)
 
     # Update the full Surface to the screen
     pygame.display.flip()
 
     # Run the program at 60 frames per second
     clock.tick(60)
+
+    queue.remove(queue[0])
+    print(state.moves)
+    
+
+
+    #calculates game state
+    #if calculateGameState(movement_player) == False:
+        #run = False
+
+    screen.fill((0,0,0))
+
+    #draws game state
+    #drawGameState(level)
+
+    # Update the full Surface to the screen
+    #pygame.display.flip()
+
+    # Run the program at 60 frames per second
+    #clock.tick(60)
 
     #break condition
     if run == False:
