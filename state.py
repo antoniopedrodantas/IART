c0f4244bf531ef503e688c5670b08fa31f974cb9
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-03-31 16:27:41 +0100

    astar much better, new heuristic

--- state.py
+++ state.py
@@ -1,19 +1,46 @@
 import pygame
 import math
+import argparse
+
+def manhattanDistance(s, start, finish):
+    return (abs(start.x - finish.x) + abs(start.y - finish.y))
+
+    
+    
+    
+def getClosestBox(s):
+
+    lowest_value_index = 0
+
+    for j in range(len(s.level.boxes)):
+        if manhattanDistance(s, s.level.player, s.level.boxes[j]) < manhattanDistance(s, s.level.player, s.level.boxes[lowest_value_index]):
+            lowest_value_index = j 
+
+    if len(s.level.boxes) == 0:
+        return manhattanDistance(s, s.level.player, s.level.finish)
+    else:
+        return manhattanDistance(s, s.level.boxes[lowest_value_index], s.level.finish)
 
 class State:
     
-    def __init__(self, level):
+    
+    def __init__(self, level, arg):
 
         self.level = level
 
         self.moves = []
+
+        self.arg = arg
+
 
     
     def addMove(self, move):
 
         self.moves.append(move)
 
-    def __lt__(self, other):
 
-        return math.sqrt(math.pow(self.level.player.x - self.level.finish.x, 2) + math.pow(self.level.player.y - self.level.finish.y, 2)) < math.sqrt(math.pow(other.level.player.x - other.level.finish.x, 2) + math.pow(other.level.player.y - other.level.finish.y, 2))
+    def __lt__(self, other):
+        if self.arg == "greedy":
+            return manhattanDistance(self, self.level.player, self.level.finish) < manhattanDistance(other, self.level.player, self.level.finish)
+        elif self.arg == "astar":
+            return getClosestBox(self) < getClosestBox(other)
